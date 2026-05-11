8123f173ce3a75b9af2a36d600bbb13b47f66b17
José Eduardo Salgado Dottori <tempjesd@gmail.com>
2026-05-11 17:19:36 -0300

    Removendo pasta mean_planner do repositório

--- mean_planner/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from mean_planner.models import Recipe, MealPlan
-
-admin.site.register(Recipe)
-admin.site.register(MealPlan)
-# Register your models here.
--- mean_planner/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MeanPlannerConfig(AppConfig):
-    name = 'mean_planner'
